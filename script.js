c8a1a11556f6cfbc8dec6f19c3d660823b6bb333
ayfor <josh.a.stubbington@gmail.com>
2021-03-06 19:29:41 -0500

    specify password length w/ verification

--- script.js
+++ script.js
@@ -12,3 +12,30 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword(){
+  console.log("Generating Password...")
+
+  //First specifiy the number of characters
+  var numberOfCharacters = +window.prompt("Please enter the number of characters in the desired password (Between 8 and 128 characters)", "8");
+
+  //Verify number of characters is between 8 and 128
+  if( numberOfCharacters < 8 || numberOfCharacters > 128 ){
+    window.alert("Invalid number of characters. Please try again.");
+    return "-";
+  }
+
+  //-----Start series of prompts to determine password criteria
+
+  var includeLowerCase;
+
+  var includeUpperCase;
+
+  var includeNumbers;
+
+  var includeSpecialCharacters;
+
+
+  
+
+}
